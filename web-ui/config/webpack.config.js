50bf7a08d0b8bdc5137c3ac4b442c742dc9af079
jorenca <mailnamefrustration@gmail.com>
2025-12-20 19:32:15 +0200

    Modify webpack config to build the js and css into a single file for each.

--- web-ui/config/webpack.config.js
+++ web-ui/config/webpack.config.js
@@ -208,14 +208,10 @@ module.exports = function (webpackEnv) {
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
-      filename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
+      filename: "static/js/main.chunk.js",
       // There are also additional JS chunk files if you use code splitting.
-      chunkFilename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].chunk.js'
-        : isEnvDevelopment && 'static/js/[name].chunk.js',
-      assetModuleFilename: 'static/media/[name].[hash][ext]',
+      //chunkFilename: "static/js/main.chunk.js",
+      assetModuleFilename: "static/media/[name][ext]",
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
@@ -247,6 +243,8 @@ module.exports = function (webpackEnv) {
     },
     optimization: {
       minimize: isEnvProduction,
+      splitChunks: false,
+      runtimeChunk: false,
       minimizer: [
         // This is only used in production mode
         new TerserPlugin({
@@ -625,8 +623,8 @@ module.exports = function (webpackEnv) {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          filename: "static/css/main.chunk.css",
+          chunkFilename: "static/css/main.chunk.css",
         }),
       // Generate an asset manifest file with the following content:
       // - "files" key: Mapping of all asset filenames to their corresponding
